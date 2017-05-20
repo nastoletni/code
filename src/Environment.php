ecb1d7d7c6ed5e69fd120e8e13e0ed88af03181b
Albert221 <w.albert221@gmail.com>
2017-05-20 21:10:49 +0200

    Add Environment

---
+++ src/Environment.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Nastoletni\Code;
+
+class Environment {
+    private $debug;
+
+    /**
+     * Environment constructor.
+     *
+     * @param bool $debug
+     */
+    public function __construct(bool $debug = false)
+    {
+        $this->debug = $debug;
+    }
+
+    /**
+     * Creates Environment from server global variables.
+     *
+     * @return Environment
+     */
+    public static function createFromGlobals()
+    {
+        $debug = in_array($_SERVER['REMOTE_ADDR'], ['::1', '127.0.0.1']);
+
+        return new static($debug);
+    }
+    
+    /**
+     * Returns if debug is enabled.
+     *
+     * @return bool
+     */
+    public function getDebug()
+    {
+        return $this->debug;
+    }
+}
