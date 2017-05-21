9a3ad4210c4212368148a2eb5dbc7b2962fe2028
Albert221 <w.albert221@gmail.com>
2017-05-21 16:18:16 +0200

    Alias server request and response in PasteController

--- src/UserInterface/Web/Controller/PasteController.php
+++ src/UserInterface/Web/Controller/PasteController.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 namespace Nastoletni\Code\UserInterface\Web\Controller;
 
 use Nastoletni\Code\UserInterface\Controller\AbstractController;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 class PasteController extends AbstractController
 {
-    public function home(ServerRequestInterface $request, ResponseInterface $response)
+    public function home(Request $request, Response $response)
     {
         return $response->getBody()->write('Paste controller, home action');
     }
