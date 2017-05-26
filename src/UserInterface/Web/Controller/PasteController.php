73e912369e9e58e1da70b205240e4e9524a5a29d
Albert221 <w.albert221@gmail.com>
2017-05-26 19:35:41 +0200

    Add basic controller support for 404

--- src/UserInterface/Web/Controller/PasteController.php
+++ src/UserInterface/Web/Controller/PasteController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Nastoletni\Code\UserInterface\Web\Controller;
 
 use Nastoletni\Code\Domain\Paste\Id;
+use Nastoletni\Code\Domain\Paste\NotExistsException;
 use Nastoletni\Code\Domain\PasteRepository;
 use Nastoletni\Code\UserInterface\Controller\AbstractController;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -44,7 +45,12 @@ class PasteController extends AbstractController
      */
     public function paste(Request $request, Response $response, string $id): Response
     {
-        $paste = $this->pasteRepository->getById(Id::createFromBase62($id));
+        try {
+            $paste = $this->pasteRepository->getById(Id::createFromBase62($id));
+        } catch (NotExistsException $e) {
+            $response->getBody()->write('404');
+            return $response->withStatus(404);
+        }
 
         return $this->twig->render($response, 'paste.twig', [
             'paste' => $paste
