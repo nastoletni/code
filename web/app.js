54c5c70eeb059d84e6557a5dfe91d42d6819b011
Bartłomiej Jacak <bartek.jacak@gmail.com>
2017-06-16 01:20:14 +0200

    Getting index from last form

--- web/app.js
+++ web/app.js
@@ -3,13 +3,11 @@ const NewFormButton = {
   LAST_FORM_QUERY: '.code-form--file:last-of-type',
 
   init() {
-    this.index = 0;
     const button = document.querySelector(this.BUTTON_QUERY);
     button.addEventListener('click', () => this.onClick());
   },
 
   onClick() {
-    this.index += 1;
     const lastForm = document.querySelector(this.LAST_FORM_QUERY);
     const newForm = this.getNewForm();
     const formsWrapper = lastForm.parentNode;
@@ -18,17 +16,23 @@ const NewFormButton = {
   },
 
   getNewForm() {
+    const index = this.getNextIndex();
     const newForm = document.createElement('fieldset');
     newForm.setAttribute('class', 'code-form--file');
-    newForm.setAttribute('data-index', this.index);
+    newForm.setAttribute('data-index', index);
 
     newForm.innerHTML =
-      `<label class="code-form--label" for="name[${this.index}]">Nazwa pliku (opcjonalna)</label>
-       <input type="text" id="name[${this.index}]" name="name[${this.index}]" class="code-form--control">
-       <label for="content[${this.index}]" class="code-form--label">Treść pliku</label>
-       <textarea name="content[${this.index}]" id="content[${this.index}]" rows="10" class="code-form--control code-form--control__textarea"></textarea>`;
+      `<label class="code-form--label" for="name[${index}]">Nazwa pliku (opcjonalna)</label>
+       <input type="text" id="name[${index}]" name="name[${index}]" class="code-form--control">
+       <label for="content[${index}]" class="code-form--label">Treść pliku</label>
+       <textarea name="content[${index}]" id="content[${index}]" rows="10" class="code-form--control code-form--control__textarea"></textarea>`;
 
     return newForm;
+  },
+
+  getNextIndex() {
+    const currentIndex = parseInt(document.querySelector(this.LAST_FORM_QUERY).getAttribute('data-index'), 10);
+    return currentIndex + 1;
   },
 };
 
