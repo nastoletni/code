cfd896f2d78b091d06779599928d745c65229bcc
Albert221 <w.albert221@gmail.com>
2017-06-15 16:29:34 +0200

    Add SymfonyValidatorExtension test

---
+++ tests/Twig/SymfonyValidatorExtensionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Nastoletni\Code\Twig;
+
+use Nastoletni\Code\Twig\SymfonyValidatorExtension;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+class SymfonyValidatorExtensionTest extends TestCase
+{
+    public function testGetFunctions()
+    {
+        $extension = new SymfonyValidatorExtension();
+        /** @var \Twig_SimpleFunction $function */
+        $function = $extension->getFunctions()[0];
+
+        $this->assertInstanceOf(\Twig_SimpleFunction::class, $function);
+        $this->assertEquals('error', $function->getName());
+    }
+
+    public function testErrorWithNull()
+    {
+        $extension = new SymfonyValidatorExtension();
+
+        $this->assertCount(0, $extension->error('test', null));
+
+    }
+
+    public function testErrorWithViolationList()
+    {
+        $extension = new SymfonyValidatorExtension();
+        $violationList = new ConstraintViolationList([
+            new ConstraintViolation('test', 'test', [], null, 'foobar', '')
+        ]);
+
+        $errors = $extension->error('foobar', $violationList);
+
+        $this->assertContains('test', $errors);
+        $this->assertCount(1, $errors);
+    }
+}
